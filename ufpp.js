03bd52b96351cff8078e85afe05c0afbc94becc7
Oliver Friedmann <oliver.friedmann@gmail.com>
2019-08-16 18:54:42 -0400

    option-value and host-ip

--- ufpp.js
+++ ufpp.js
@@ -1,5 +1,17 @@
 const Readline = require('readline');
 const FS = require('fs');
+const GetOpt = require("node-getopt");
+const OS = require("os");
+
+var parsedArgs = GetOpt.create([
+    ["o", "customargs=ARG+", "custom arguments"]
+]).bindHelp().parseSystem();
+
+var customArgs = {};
+(parsedArgs.options.customargs || []).forEach(function (keyvalue) {
+    var splt = keyvalue.split(":");
+    customArgs[splt.shift()] = splt.join(":");
+});
 
 const commands = {
     "file-read": function (args, options) {
@@ -10,6 +22,32 @@ const commands = {
             });
         }
         return body.join("\n");
+    },
+    "option-value": function (args, options) {
+        return customArgs[args.key];
+    },
+    "host-ip": function (args, options) {
+        var ifaces = OS.networkInterfaces();
+        var ips = [];
+        Object.keys(ifaces).forEach(function(dev) {
+            for (var i = 0, len = ifaces[dev].length; i < len; i++) {
+                var details = ifaces[dev][i];
+                if (details.family === 'IPv4')
+                    ips.push(details.address);
+            }
+        });
+        var hostip = null;
+        ips.forEach(function (ip) {
+            if (hostip)
+                return;
+            if (ip.indexOf("192.") === 0 || ip.indexOf("10.") === 0 || ip.indexOf("172.") === 0 || (ip.indexOf("127.") === 0 && ip !== "127.0.0.1"))
+                hostip = ip;
+        });
+        if (!hostip && ips.length > 0)
+            hostip = ips[0];
+        if (!hostip)
+            hostip = "127.0.0.1";
+        return hostip;
     }
 };
 
